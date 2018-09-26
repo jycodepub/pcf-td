e6817b74ce25ef9e7692e331adb5e5d4b9034af4
Jeff Yeh <jeff.yeh@fiserv.com>
2018-09-26 12:57:42 -0500

    Test Docker Deployment
    1. Updated version to 1.1.0
    2. Removed unused import from application class
    3. Correct error in docker-compose.yml for Rabbitmq service

--- src/main/kotlin/com/fiserv/edd/pcftd/PcfTdApplication.kt
+++ src/main/kotlin/com/fiserv/edd/pcftd/PcfTdApplication.kt
@@ -1,13 +1,7 @@
 package com.fiserv.edd.pcftd
 
-import org.springframework.amqp.core.*
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
 
 @SpringBootApplication
 class PcfTdApplication {
